a7034e6f6fee752c192ba5dd560853415f1a6017
febrimaulana <febriyunus@gmail.com>
2021-10-17 13:05:49 +0700

    add login done

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,54 @@
 import React from "react";
+import {
+  Button,
+  ButtonToolbar,
+  Container,
+  Content,
+  FlexboxGrid,
+  Form,
+  Panel,
+} from "rsuite";
+import { Image } from "semantic-ui-react";
+import { ILLogo } from "../../assets";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Hallo</h1>
+    <div className="show-fake-browser login-page">
+      <Container>
+        <Content>
+          <Image
+            src={ILLogo}
+            size="medium"
+            centered
+            style={{ marginTop: 50, marginBottom: 20 }}
+          />
+          <FlexboxGrid justify="center">
+            <FlexboxGrid.Item colspan={12}>
+              <Panel header={<h3>Login</h3>} bordered>
+                <Form fluid>
+                  <Form.Group>
+                    <Form.ControlLabel>email address</Form.ControlLabel>
+                    <Form.Control name="name" />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.ControlLabel>Password</Form.ControlLabel>
+                    <Form.Control
+                      name="password"
+                      type="password"
+                      autoComplete="off"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <ButtonToolbar>
+                      <Button appearance="primary">Login</Button>
+                    </ButtonToolbar>
+                  </Form.Group>
+                </Form>
+              </Panel>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
+        </Content>
+      </Container>
     </div>
   );
 };
